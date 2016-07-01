ffa3e87b03641d46bc9f70a64dd6625508eb2371
Juan Carlos Ruiz <JuanCrg90@gmail.com>
2016-07-01 00:26:35 -0500

    Add Board Class and testing

---
+++ test/board-test.js
@@ -0,0 +1,72 @@
+// Unit Test for Board Class
+import test from 'tape';
+import Board from '../lib/board';
+import Cell from '../lib/cell';
+
+test('Should exists', (t) => {
+  const board = new Board(10, 10);
+
+  t.ok(Board, 'Board should exists');
+  t.equals(typeof Board, 'function', 'Board should be a function');
+  t.ok(board instanceof Board, 'board should be an instance of board');
+  t.end();
+});
+
+test('Has width setter', (t) => {
+  const board = new Board(10, 10);
+
+  t.ok(board.setWidth, 'Should exists');
+  t.end();
+});
+
+test('Has width getter', (t) => {
+  const board = new Board(10, 10);
+
+  t.ok(board.getWidth, 'Should exists');
+  t.end();
+});
+
+test('Has heigth setter', (t) => {
+  const board = new Board(10, 10);
+
+  t.ok(board.setHeigth, 'Should exists');
+  t.end();
+});
+
+test('Has heigth getter', (t) => {
+  const board = new Board(10, 10);
+
+  t.ok(board.getHeigth, 'Should exists');
+  t.end();
+});
+
+test('Has grid setter', (t) => {
+  const board = new Board(10, 10);
+
+  t.ok(board.setGrid, 'Should exists');
+  t.end();
+});
+
+test('Has grid getter', (t) => {
+  const board = new Board(10, 10);
+
+  t.ok(board.getGrid, 'Should exists');
+  t.end();
+});
+
+test('Can get a cell in the grid', (t) => {
+  const board = new Board(10, 10);
+
+  t.ok(board.getCell, 'Should exists');
+  t.ok(board.getCell(0, 0) instanceof Cell, 'Should return a Cell Object');
+  t.end();
+});
+
+test('Can Update a cell in the grid', (t) => {
+  const board = new Board(10, 10);
+  const cell = board.getCell(0, 0);
+
+  t.ok(board.updateCell, 'Should exist');
+  t.notEqual(cell.isAlive(), board.updateCell(0, 0, !cell.isAlive()), 'Should be nor equal');
+  t.end();
+});
